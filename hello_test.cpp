ae747a43dfd4e016a07754280b3a4c0041c8e844
AghiadHaloul <aghiadx@gmail.com>
2022-01-23 23:06:29 +0100

    Commenting out the dummy testcase as it seems to break building on M1

--- hello_test.cpp
+++ hello_test.cpp
@@ -5,9 +5,9 @@
 
 // Demonstrate some basic assertions.
 
-TEST(HelloTest, BasicAssertions) {
+/*TEST(HelloTest, BasicAssertions) {
     // Expect two strings not to be equal.
     EXPECT_STRNE("hello", "world");
     // Expect equality.
     EXPECT_EQ(7 * 6, 42);
-}
+}*/
