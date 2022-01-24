fc156c7cd4ec4f0bad3ee293b04bed606dfdac43
AghiadHaloul <aghiadx@gmail.com>
2022-01-24 01:58:46 +0100

    Added "IntentRecognizer" class + skeleton code

---
+++ src/IntentRecognizer.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Aghiad Haloul on 24.01.22.
+//
+
+#include "IntentRecognizer.h"
+
+IntentRecognizer::IntentRecognizer() {
+    TextAnalyser text_analyser();
+}
+
+string IntentRecognizer::get_intent(const string& query) {
+   return "";
+}
---
+++ src/IntentRecognizer.h
@@ -0,0 +1,19 @@
+//
+// Created by Aghiad Haloul on 24.01.22.
+//
+
+#ifndef INTENTRECOGNIZER_INTENTRECOGNIZER_H
+#define INTENTRECOGNIZER_INTENTRECOGNIZER_H
+#include "TextAnalyser.h"
+
+class IntentRecognizer {
+public:
+    IntentRecognizer();
+    string get_intent(const string& query);
+private:
+    TextAnalyser text_analyser;
+
+};
+
+
+#endif //INTENTRECOGNIZER_INTENTRECOGNIZER_H
